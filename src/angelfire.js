6bd53ae9f2b3399e70fc5d8b158831d436e6af7c
rish-16 <mail.rishabh.anand@gmail.com>
2020-05-28 15:20:54 +0800

    Updates

--- src/angelfire.js
+++ src/angelfire.js
@@ -5,37 +5,27 @@ Angelfire.prototype.addMenu = (element, config) => {
 	menu.className = "menu"
 	var menuVisible = false
 	
-	menu.style.width = "150px"
-	menu.style.boxShadow = "1px 1px 5px rgba(0, 0, 0, 0.2)"
-	menu.style.borderRadius = "7px"
-	menu.style.position = "relative"
-	menu.style.display = "none"
-	
 	var ul = document.createElement("ul")
 	ul.className = "menu-options"
 	
-	ul.style.listStyle = "none"
-	ul.style.padding = "10px 0"
-	
 	const toggleMenu = command => {
-		menu.style.display = command === "show" ? "block" : "none";
+		menu.style.display = command === "show" ? "block" : "none"
 	}
 	
 	const setPosition = ({ top, left }) => {
-		menu.style.left = `${left}px`;
-		menu.style.top = `${top}px`;
-		toggleMenu('show');
+		menu.style.left = `${left}px`
+		menu.style.top = `${top}px`
+		toggleMenu('show')
 	};
 	
-	menu.innerHTML = ""
-	ul.innerHTML = ""
 	element.addEventListener("contextmenu", (e) => {
-		e.preventDefault();
+		e.preventDefault()
+		console.log(e.pageX, e.pageY)
 		const origin = {
 			left: e.pageX,
 			top: e.pageY
-		};
-		setPosition(origin);
+		}
+		setPosition(origin)
 		menuVisible = true
 		  
 		var allOptions = config["options"]
@@ -48,28 +38,20 @@ Angelfire.prototype.addMenu = (element, config) => {
 			var li = document.createElement("li")
 			li.className = "menu-option"
 			li.textContent = record["name"]
+			li.id = `option-${i}`
 			
-			li.style.fontWeight = "500"
-			li.style.fontSize = "14px"
-			li.style.padding = "10px 40px 10px 20px"
-			li.style.cursor = "pointer"
-			
-			li.addEventListener("mouseover", () => {
-				li.style.background = "rgba(0, 0, 0, 0.1)"
-			})
+			li.onclick = () => {
+				record["onTrigger"]()
+			}
 			
 			ul.appendChild(li)
-			
-			li.addEventListener(record['trigger'], () => {
-				var func = record["onTrigger"]
-				func()
-			})
 		}
 	})
 	
 	window.addEventListener("click", e => {
-		if(menuVisible)toggleMenu("hide");
-	});
+		if (menuVisible) {toggleMenu("hide")}
+		ul.innerHTML = ""
+	})
 	
 	menu.appendChild(ul)
 	document.body.appendChild(menu)
--- src/app.js
+++ src/app.js
@@ -8,21 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
 		"options": [
 		{
 			"name": "Add",
-			"trigger": "click",
 			"onTrigger": function() {
 				alert("Add clicked!")
 			}
 		},
 		{
 			"name": "Delete",
-			"trigger": "click",
 			"onTrigger": function() {
 				alert("Delete clicked!")
 			}
 		},
 		{
 			"name": "Subtract",
-			"trigger": "click",
 			"onTrigger": function() {
 				alert("Subtract clicked!")
 			}
